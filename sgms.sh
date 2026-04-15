3f3b1589fe09f8361f90efd309d8bb42d3d23a6e
elftiany <mk5920201@gmail.com>
2026-04-15 20:31:41 +0200

    Created Main Menu

---
+++ sgms.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/bash
+
+shopt -s extglob
+
+mainmenu(){
+while true; do
+    echo ""
+    echo " ---- Bash SGMS ----"
+
+	select choice in \
+		"▸ManageStudents" \
+		"▸ManageSubjects" \
+		"▸ManageGrades" \
+		"▸Reports&Statistics" \
+		"▸Exit"
+	do
+		case $REPLY in 
+	1)  	 ManageStudents ;;
+	2)	 ManageSubjects ;;
+	3) 	 ManageGrades ;;
+	4)	 Reports_Statistics ;;
+	5)	 exit ;;
+		
+	*)
+		echo "Invalid option"
+	esac
+			
+ 	break
+	done
+
+done
+}
+mainmenu
