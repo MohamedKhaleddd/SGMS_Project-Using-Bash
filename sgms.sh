65f6ac14d1fef72a5750a74a849e2b230f63a9e5
elftiany <mk5920201@gmail.com>
2026-04-19 00:55:11 +0200

    Created Top Students by GPA function using sort -2k to sort gpa with in field num 2

--- sgms.sh
+++ sgms.sh
@@ -414,6 +414,48 @@ awk -F'|' '
 
 
 
+TopStudentsByGPA(){
+
+    echo "----- Top Students by GPA -----"
+
+    for stu in "$std_data_dir"/*.stu
+    do
+        sid=$(sed -n '1p' "$stu")
+        name=$(sed -n '2p' "$stu")
+        total=0
+        count=0
+
+        for grd in "$grade_dir"/*.grd
+        do
+            line=$(grep "^$sid|" "$grd")
+            if [ ! -z "$line" ]; then
+                grade=$(echo "$line" | cut -d'|' -f3)
+                if   [ "$grade" = "A" ] || [ "$grade" = "A+" ]; then gp=4
+                elif [ "$grade" = "A-" ]; then gp=3.7
+                elif [ "$grade" = "B+" ]; then gp=3.3
+                elif [ "$grade" = "B"  ]; then gp=3
+                elif [ "$grade" = "B-" ]; then gp=2.7
+                elif [ "$grade" = "C+" ]; then gp=2.3
+                elif [ "$grade" = "C"  ]; then gp=2
+                elif [ "$grade" = "C-" ]; then gp=1.7
+                elif [ "$grade" = "D"  ]; then gp=1
+                else gp=0
+                fi
+                total=$(echo "$total + $gp" | bc)
+                count=$((count + 1))
+            fi
+        done
+
+        if [[ $count > 0 ]]; then
+        gpa=$(echo "scale=2; $total / $count" | bc)
+            echo "$name $gpa"
+        fi
+
+    done | sort -k2 -nr
+}
+
+
+
 
 Reports_Statistics(){
 while true
