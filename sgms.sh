fde2b6f0b43f050274c95904b1792623a17c9971
elftiany <mk5920201@gmail.com>
2026-04-18 17:19:25 +0200

    Created Subject statistics and calculate (max,min,avg) with subject's code by using awk

--- sgms.sh
+++ sgms.sh
@@ -357,6 +357,59 @@ StudentTranscriptGPA(){
 
 
 
+SubjectStatistics(){
+
+   read -p "Enter subject code: " code
+
+    file="$subject_dir/$code.sub"
+
+    if [[ ! -f "$file" ]]
+    then
+        echo "Subject not found"
+        return
+    fi
+
+    name=$(sed -n '2p' "$file")
+    credits=$(sed -n '3p' "$file")
+
+    echo "----- Subject Info -----"
+    echo "Code: $code"
+    echo "Name: $name"
+    echo "Credits: $credits"
+
+    grd_file="$grade_dir/$code.grd"
+
+    if [[ ! -f "$grd_file" ]]
+    then
+        echo "No grades recorded for this subject"
+        return
+    fi
+
+    echo ""
+    echo "----- Statistics -----"
+
+
+awk -F'|' '
+        {
+
+                sum += $2
+                if (NR == 1 ||  $2 > max) max = $2
+                if (NR == 1 || $2 < min) min = $2
+        }
+        
+        END {
+                if (NR >0){
+                print "Average = " sum/NR
+                print "Max = " max
+                print "Min = " min
+                }
+                else {
+                        print "no data"
+                        }
+        } ' "$grd_file"
+}
+
+
 
 
 
